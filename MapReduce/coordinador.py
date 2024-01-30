f6779d959bda330b373f8d1c17a978ea09a9a55c
CesarCardenasD <112493385+CesarCardenasD@users.noreply.github.com>
2024-01-29 21:42:38 -0500

    Coordinador y Map
    Separación procesos en coordinador

--- MapReduce/coordinador.py
+++ MapReduce/coordinador.py
@@ -14,9 +14,9 @@ import multiprocessing
 import random
 import threading 
 from threading import current_thread
-import time
 
-#from map import map_execution
+
+from map import map_execution
 
 nodes_cores = multiprocessing.cpu_count()
 print(f"CORES -> {nodes_cores} \t")
@@ -62,37 +62,7 @@ def split_file(clean_file, chunks_folder, num_chunks):
 
             chunk_number += 1
 
-#Funcion Map
-def map_execution(list_chunk_name,lock,error):
-    #Sacar el nombre del hilo que se esta ejecutando
-    id = current_thread().name
-    
-    if error==True:
-            print(f"Reiniciando Hilo {id} Dañado")
-            time.sleep(15)
-            print(f"Hilo {id} Reparado, Iniciando Procesos")
-    chunk_name=""
-    while len(list_chunk_name)>0:
-        
-        with lock:
-            chunk_name=list_chunk_name.pop(0)
-        
-        
-        #Inicia Proceso
-        print(f"MAP Nodo {id} inicio el proceso del Chunk: {chunk_name}")
-        
-        #Lectura del archivo chunk
-        with open(f"ficheros/chunks/{chunk_name}", "r", encoding='utf-8') as reader:
-            words = reader.read().split(" ")
-            
-            #Escritura del mapeado del archivo chunk
-            for word in words:
-                with open(f"ficheros/mapped_chunks/mapped_{chunk_name}", "a+", encoding='utf-8') as writer:
-                    writer.write(f"<{word}, 1>\n")
-        #Finaliza Proceso
-        print(f"MAP Nodo {id} finalizo su proceso de: {chunk_name}")
 
-    return None
 
 def ejecutar_hilos(lista, ErrorNodoMap):
     id = current_thread().name
--- MapReduce/map.py
+++ MapReduce/map.py
@@ -1,24 +1,35 @@
-from threading import Thread, current_thread
-
+from threading import current_thread
+import time
 
 #Funcion Map
-def map_execution(chunk_number):
+def map_execution(list_chunk_name,lock,error):
     #Sacar el nombre del hilo que se esta ejecutando
     id = current_thread().name
     
-    #Inicia Proceso
-    print(f"MAP Nodo {id} inicio el proceso del Chunk: {chunk_number}")
-    
-    #Lectura del archivo chunk
-    with open(f"ficheros/chunks/{chunk_number}", "r") as reader:
-        words = reader.read().split(" ")
+    if error==True:
+            print(f"Reiniciando Hilo {id} Dañado")
+            time.sleep(15)
+            print(f"Hilo {id} Reparado, Iniciando Procesos")
+    chunk_name=""
+    while len(list_chunk_name)>0:
         
-        #Escritura del mapeado del archivo chunk
-        for word in words:
-            with open(f"ficheros/mapped_chunks/mapped_{chunk_number}", "a+") as writer:
-                writer.write(f"<{word}, 1>\n")
-    #Finaliza Proceso
-    print(f"MAP Nodo {id} finalizo su proceso de: {chunk_number}")
+        with lock:
+            chunk_name=list_chunk_name.pop(0)
+        
+        
+        #Inicia Proceso
+        print(f"MAP Nodo {id} inicio el proceso del Chunk: {chunk_name}")
+        
+        #Lectura del archivo chunk
+        with open(f"ficheros/chunks/{chunk_name}", "r", encoding='utf-8') as reader:
+            words = reader.read().split(" ")
+            
+            #Escritura del mapeado del archivo chunk
+            for word in words:
+                with open(f"ficheros/mapped_chunks/mapped_{chunk_name}", "a+", encoding='utf-8') as writer:
+                    writer.write(f"<{word}, 1>\n")
+        #Finaliza Proceso
+        print(f"MAP Nodo {id} finalizo su proceso de: {chunk_name}")
 
     return None
 
